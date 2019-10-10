0df0f23d7c7e13d303b5e98e2b42720d4b589b32
Anton Kozlov <akozlov@azul.com>
2019-10-10 20:38:42 +0300

    Fix emu-user build script

--- dev-riscv/emulator-userspace/scripts/bootstrap.sh
+++ dev-riscv/emulator-userspace/scripts/bootstrap.sh
@@ -7,5 +7,5 @@ rootfs="/riscv"
 apt-get update
 apt-get install -y qemu-user-static debootstrap debian-ports-archive-keyring
 mkdir -p $rootfs/usr/bin
-cp /usr/bin/qemu-riscv64-static $rootfs/usr/bin
 debootstrap --arch=riscv64 --keyring /usr/share/keyrings/debian-ports-archive-keyring.gpg --include=debian-ports-archive-keyring unstable $rootfs/ http://deb.debian.org/debian-ports
+cp /usr/bin/qemu-riscv64-static $rootfs/usr/bin
