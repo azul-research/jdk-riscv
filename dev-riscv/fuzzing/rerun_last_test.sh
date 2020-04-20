e42a4d9730170ba0136d02b64d17580945a7d8a6
Nikita Tsarev <nikts53@gmail.com>
2020-04-20 13:33:07 +0300

    Rounding modes according to JVMS

--- dev-riscv/fuzzing/rerun_last_test.sh
+++ dev-riscv/fuzzing/rerun_last_test.sh
@@ -4,14 +4,16 @@ variant=core
 level=slowdebug
 jdk_riscv_path=$(pwd)/../..
 sysroot_path=$jdk_riscv_path/riscv-sysroot
+quiet=
 
 OPTIND=1
 
-while getopts "hv:l:" opt; do
+while getopts "hqv:l:" opt; do
     case "$opt" in
     h)
         echo "usage: $0 [-h] [-v variant] [-l debug-level]"
         echo "       -h show help"
+        echo "       -q do not trace bytecodes"
         echo "       -v choose jvm-variant (server, client, minimal, core, zero, zeroshark, custom). default is core"
         echo "       -l choose debug level (release, fastdebug, slowdebug, optimized). default is release"
         exit 0
@@ -20,8 +22,15 @@ while getopts "hv:l:" opt; do
         ;;
     l)  level=$OPTARG
         ;;
+    q)  quiet=1
+        ;;
     esac
 done
+
+extraopts=
+if [ "x$quiet" = "x" ]; then
+    extraopts="-XX:+TraceBytecodes -XX:+Verbose"
+fi
 
 javac javafuzz/T1.java
 java javafuzz.T1 > answer
@@ -38,6 +47,6 @@ docker run --rm -it \
     /bin/bash -c "cp /jdk-riscv/dev-riscv/fuzzing/javafuzz/T1.class \
     /jdk-riscv/build/linux-riscv64-$variant-$level/jdk/modules/java.base/javafuzz/ && \
     /jdk-riscv/build/linux-riscv64-$variant-$level/jdk/bin/java \
-    -XX:+CallTestMethod -XX:+TraceBytecodes -XX:+Verbose \
+    -XX:+CallTestMethod $extraopts \
     -XX:TestMethodClass=javafuzz.T1 -XX:TestMethodName=test \
     -XX:+ExitAfterTestMethod" || exit 1
